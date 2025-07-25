db99e5fa54cac2136ae0bc3de4d87c1d2c5e15ba
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-25 13:22:12 -0300

    010

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -19,8 +19,13 @@ class MainController extends Controller
         return view('home', $data);
     }
 
-    public function submitForm(): void
+    public function submitForm(Request $request): void
     {
-        echo "Formulario submetido!";
+        $request->validate([
+            'name' => 'required',
+            'country' => 'required|min:6'
+        ]);
+
+        echo 'Formulário submetido com sucesso';
     }
 }
