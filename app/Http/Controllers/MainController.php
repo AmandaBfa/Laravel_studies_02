72ac59d5f25bfabe56e9037a6f3dbca0a729d981
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-25 08:52:05 -0300

    002

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,6 +9,32 @@ class MainController extends Controller
 {
     public function showView(): View
     {
-        return view('admin.newPage3');
+        // método 1
+        // $data = [
+        //     'name' => "Amanda Barbosa",
+        //     'phone' => "123456789"
+        // ];
+        // return view('admin.newPage3', $data); <== melhor maneira
+        // return view('admin.newPage3', [
+        //     'name' => "Amanda Barbosa",
+        //     'phone' => "123456789"
+        // ]);
+
+        // // método 2
+        // return view('admin.newPage3', [
+        //     'name' => "Amanda Barbosa",
+        //     'phone' => "123456789"
+        // ]);
+
+        // // método 2
+        // return view('admin.newPage3')
+        //     ->with('name', "Amanda Barbarosa")
+        //     ->with('phone', "62901011213");
+
+        // método 3
+        $name = 'Amanda Barbarosa';
+        $phone = '62901011213';
+
+        return view('admin.newPage3', compact('name', 'phone'));
     }
 }
