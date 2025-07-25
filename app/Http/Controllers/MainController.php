e54511d89588d29bec624634a8a622b9d1b76d31
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-07-25 09:34:50 -0300

    003

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,32 +9,6 @@ class MainController extends Controller
 {
     public function showView(): View
     {
-        // método 1
-        // $data = [
-        //     'name' => "Amanda Barbosa",
-        //     'phone' => "123456789"
-        // ];
-        // return view('admin.newPage3', $data); <== melhor maneira
-        // return view('admin.newPage3', [
-        //     'name' => "Amanda Barbosa",
-        //     'phone' => "123456789"
-        // ]);
-
-        // // método 2
-        // return view('admin.newPage3', [
-        //     'name' => "Amanda Barbosa",
-        //     'phone' => "123456789"
-        // ]);
-
-        // // método 2
-        // return view('admin.newPage3')
-        //     ->with('name', "Amanda Barbarosa")
-        //     ->with('phone', "62901011213");
-
-        // método 3
-        $name = 'Amanda Barbarosa';
-        $phone = '62901011213';
-
-        return view('admin.newPage3', compact('name', 'phone'));
+        return view('home', ['value' => 1000]); // altera esse valor do value ai
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
 @extends('layouts/main_layout')
 @section('content')
-    <p class="display-1 text-center"> Hellor World! </p>
 @endsection
